e9a1764b2f7f226550d84b31f811c1f9781af6c8
hamed-rezaee <hamed.rezaee@outlook.com>
2020-03-13 00:47:11 +0330

    - add main axis logic

--- lib/main.dart
+++ lib/main.dart
@@ -28,15 +28,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool _isFirstPersonTurn = true;
-
+  static bool _isFirstPersonTurn = true;
   static Color _backgroundColor = Colors.green;
 
-  final List<List<Widget>> _isSelected = List.generate(
+  final List<List<Container>> _board = List.generate(
     8,
     (int row) => List.generate(
       8,
-      (column) => Center(),
+      (column) => row == 3 && column == 3
+          ? _buildBlockUnit(false)
+          : row == 3 && column == 4
+              ? _buildBlockUnit(true)
+              : row == 4 && column == 3
+                  ? _buildBlockUnit(true)
+                  : row == 4 && column == 4 ? _buildBlockUnit(false) : null,
     ),
   );
 
@@ -60,7 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     fontSize: 16.0,
                   ),
                 ),
-                _buildBlockUnit(_isFirstPersonTurn, 32.0),
+                _buildBlockUnit(_isFirstPersonTurn),
               ],
             ),
           ),
@@ -73,12 +78,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   List<Widget> _generatePlayground() {
     List<Widget> result = List<Widget>();
-    double size = MediaQuery.of(context).size.width / 8 - 4;
+    double size = 32.0;
 
-    for (int i = 0; i < 8; i++) {
+    for (int row = 0; row < 8; row++) {
       List<Widget> children = List<Widget>();
 
-      for (int j = 0; j < 8; j++) {
+      for (int column = 0; column < 8; column++) {
         children.add(
           Row(
             children: <Widget>[
@@ -90,9 +95,12 @@ class _MyHomePageState extends State<MyHomePage> {
                     color: _backgroundColor,
                     borderRadius: BorderRadius.circular(4.0),
                   ),
-                  child: _isSelected[i][j],
+                  child: Padding(
+                    padding: const EdgeInsets.all(3.0),
+                    child: _board[row][column],
+                  ),
                 ),
-                onTap: () => _changeChild(i, j),
+                onTap: () => _changeChild(row, column),
               ),
               Padding(
                 padding: const EdgeInsets.only(right: 2.0),
@@ -118,27 +126,185 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _changeChild(int row, int column) {
-    double size = MediaQuery.of(context).size.width / 8;
-
-    if (_isSelected[row][column] is Center) {
-      _isSelected[row][column] = _buildBlockUnit(_isFirstPersonTurn, size);
+    if (_isEmpty(row, column) &&
+        (_isValidTop(row, column) ||
+            _isValidBottom(row, column) ||
+            _isValidLeft(row, column) ||
+            _isValidRight(row, column))) {
+      _board[row][column] = _buildBlockUnit(_isFirstPersonTurn);
       _isFirstPersonTurn = !_isFirstPersonTurn;
 
       setState(() {});
     }
   }
 
-  Widget _buildBlockUnit(bool isFirstPersonTurn, double size) {
-    return Padding(
-      padding: const EdgeInsets.all(4.0),
-      child: Container(
-        width: size,
-        height: size,
-        decoration: BoxDecoration(
-          color: _isFirstPersonTurn ? Colors.black : Colors.white,
-          borderRadius: BorderRadius.circular(50.0),
-        ),
+  static Widget _buildBlockUnit(bool isFirstPersonTurn) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      width: 32.0,
+      height: 32.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50.0),
+        color: isFirstPersonTurn ? Colors.black : Colors.white,
       ),
     );
+  }
+
+  bool _isEmpty(int row, int column) => _board[row][column] == null;
+
+  bool _isValidTop(int row, int column) {
+    if (row - 2 < 0) {
+      return false;
+    }
+
+    if (_board[row - 1][column] == null) {
+      return false;
+    }
+
+    if ((_board[row - 1][column]?.decoration as BoxDecoration).color ==
+        (_isFirstPersonTurn ? Colors.black : Colors.white)) {
+      return false;
+    }
+
+    for (int i = row - 2; i > 0; i--) {
+      Container child = _board[i][column];
+
+      if (child == null) {
+        return false;
+      }
+
+      if ((child.decoration as BoxDecoration).color ==
+          (_isFirstPersonTurn ? Colors.black : Colors.white)) {
+        _changeTopColor(row, column);
+
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  bool _isValidBottom(int row, int column) {
+    if (row + 2 > 7) {
+      return false;
+    }
+
+    if (_board[row + 1][column] == null) {
+      return false;
+    }
+
+    if ((_board[row + 1][column]?.decoration as BoxDecoration).color ==
+        (_isFirstPersonTurn ? Colors.black : Colors.white)) {
+      return false;
+    }
+
+    for (int i = row + 2; i < 8; i++) {
+      Container child = _board[i][column];
+
+      if (child == null) {
+        return false;
+      }
+
+      if ((child.decoration as BoxDecoration).color ==
+          (_isFirstPersonTurn ? Colors.black : Colors.white)) {
+        _changeButtomColor(row, column);
+
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  bool _isValidLeft(int row, int column) {
+    if (column - 2 < 0) {
+      return false;
+    }
+
+    if (_board[row][column - 1] == null) {
+      return false;
+    }
+
+    if ((_board[row][column - 1]?.decoration as BoxDecoration).color ==
+        (_isFirstPersonTurn ? Colors.black : Colors.white)) {
+      return false;
+    }
+
+    for (int i = column - 2; i > 0; i--) {
+      Container child = _board[row][i];
+
+      if (child == null) {
+        return false;
+      }
+
+      if ((child.decoration as BoxDecoration).color ==
+          (_isFirstPersonTurn ? Colors.black : Colors.white)) {
+        _changeLeftColor(row, column);
+
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  bool _isValidRight(int row, int column) {
+    if (column + 2 > 7) {
+      return false;
+    }
+
+    if (_board[row][column + 1] == null) {
+      return false;
+    }
+
+    if ((_board[row][column + 1]?.decoration as BoxDecoration).color ==
+        (_isFirstPersonTurn ? Colors.black : Colors.white)) {
+      return false;
+    }
+
+    for (int i = column + 2; i < 8; i++) {
+      Container child = _board[row][i];
+
+      if (child == null) {
+        return false;
+      }
+
+      if ((child.decoration as BoxDecoration).color ==
+          (_isFirstPersonTurn ? Colors.black : Colors.white)) {
+        _changeRightColor(row, column);
+
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  void _changeTopColor(int row, int column) {
+    while ((_board[--row][column].decoration as BoxDecoration).color !=
+        (_isFirstPersonTurn ? Colors.black : Colors.white)) {
+      _board[row][column] = _buildBlockUnit(_isFirstPersonTurn);
+    }
+  }
+
+  void _changeButtomColor(int row, int column) {
+    while ((_board[++row][column].decoration as BoxDecoration).color !=
+        (_isFirstPersonTurn ? Colors.black : Colors.white)) {
+      _board[row][column] = _buildBlockUnit(_isFirstPersonTurn);
+    }
+  }
+
+  void _changeLeftColor(int row, int column) {
+    while ((_board[row][--column].decoration as BoxDecoration).color !=
+        (_isFirstPersonTurn ? Colors.black : Colors.white)) {
+      _board[row][column] = _buildBlockUnit(_isFirstPersonTurn);
+    }
+  }
+
+  void _changeRightColor(int row, int column) {
+    while ((_board[row][++column].decoration as BoxDecoration).color !=
+        (_isFirstPersonTurn ? Colors.black : Colors.white)) {
+      _board[row][column] = _buildBlockUnit(_isFirstPersonTurn);
+    }
   }
 }
